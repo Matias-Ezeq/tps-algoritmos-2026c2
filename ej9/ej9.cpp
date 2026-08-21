22c163373fefc81291e135096928009c75c476a9
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-08-21 18:30:50 -0300

    Correción del comentario explicando la lógica

--- ej9/ej9.cpp
+++ ej9/ej9.cpp
@@ -12,7 +12,7 @@ int main(){
     cout << "ingrese dos números separados por un espacio: ";
     cin >> numero1 >> numero2;
 
-    //c++ toma enteros positivos como verdadero y 0 como falso. módulo 2 retorna 0 si el numero es par y 1 si es impar
+    //c++ toma enteros positivos como verdadero y 0 como falso. módulo N retorna 0 si el numero es divisible por N y >0 si no
     if (numero1 % numero2){
         resultado = "no es divisible";
     }
