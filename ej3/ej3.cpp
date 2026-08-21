8cc20d5e5213f4e4e9ba048bb887f409590b91d9
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-08-21 14:55:24 -0300

    Ejercicio 3

---
+++ ej3/ej3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+/*
+    3) Escribir un programa que lea el nombre de una persona y luego lo salude.
+*/
+
+int main()
+{
+    string nombre = "";
+    cout << "Ingrese su nombre: ";
+    getline(cin, nombre);
+    cout << "Hola " << nombre << "!\n";
+    return 0;
+}
