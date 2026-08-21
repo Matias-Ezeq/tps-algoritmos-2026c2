f9ee30e5e902f1be02f458376143ceed451bf146
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-08-21 16:25:44 -0300

    Ejercicio 6 (consultar lo del comentario en linea 27 de ej6.cpp)

---
+++ ej6/ej6.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+/*
+    6) Leer un número real y decir si es mayor, menor o igual a cero.
+*/
+
+int main() {
+    double numero = 0;
+    string resultado = "el if no funca";
+
+    cout << "Ingrese un número real: ";
+    cin >> numero;
+
+    if (numero > 0){
+        resultado = "mayor";
+    }
+    else if (numero < 0){
+        resultado = "menor";
+    }
+    else {
+        resultado = "igual";
+    }
+
+    printf("%g es %s a 0\n",numero,resultado.c_str());
+    //no se por qué es necesario castear resultado a 'char *' pero si no lo hago printf() muestra basura
+
+    return 0;
+}
