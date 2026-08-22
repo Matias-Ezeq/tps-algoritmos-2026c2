d8a5056a63260c695f95a56fa06defdc64f2c126
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-08-21 21:14:29 -0300

    Ejercicio 14

---
+++ ej14/ej14.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+/*
+    14) Leer un número N y calcular su factorial.
+*/
+
+int main() {
+    int numero, resultado = 1;
+    cout << "Ingrese un número: ";
+    cin >> numero;
+
+    for (int i = numero; i > 0; i--){
+        resultado = resultado * i;
+    }
+
+    printf("%d! = %d",numero,resultado);
+
+    return 0;
+}
