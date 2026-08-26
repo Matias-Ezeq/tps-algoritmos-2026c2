d9aa746521c0cc4ab43340aeb8c555d9be5d68cd
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-08-26 14:32:55 -0300

    Ejercicio 15

---
+++ ej15/ej15.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+/*
+    Leer una serie de números reales, terminando la serie con un cero.
+    Imprimir los datos a medida que se los ingresa junto con la suma parcial de los mismos.
+*/
+
+int main() {
+
+    float in = 1;
+    float suma = 0;
+
+    while (in != 0) {
+        cout << "Ingrese número real: ";
+        cin >> in;
+        suma += in;
+        printf("Número ingresado: %g\nSuma acumulada: %g\n",in,suma);
+    }
+
+
+    return 0;
+}
