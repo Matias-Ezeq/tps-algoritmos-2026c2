4ece823a213c448598ba8e32eeb5c9c947e9d8eb
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-09-04 15:24:02 -0300

    Ejercicio 25

--- ej25/ej25.cpp
+++ ej25/ej25.cpp
@@ -9,12 +9,37 @@ using namespace std;
 struct raices{
     float raiz1;
     float raiz2;
+};
+float calcularRaiz1(float a, float b, float c) {
+    float raiz = 0;
+
+    raiz = (-b + sqrt( b*b - 4*a*c)) / (2*a);
+
+    return (raiz);
+}
+
+float calcularRaiz2(float a, float b, float c) {
+    float raiz = 0;
+
+    raiz = (-b - sqrt( b*b - 4*a*c)) / (2*a);
+
+    return (raiz);
 }
 
 raices calcularRaices(float a, float b, float c){
     raices raices;
+    raices.raiz1 = calcularRaiz1(a,b,c);
+    raices.raiz2 = calcularRaiz2(a,b,c);
 
-};
+    if (!isfinite(raices.raiz1)){
+        raices.raiz1 = 0;
+    }
+    if (!isfinite(raices.raiz2)){
+        raices.raiz2 = 0;
+    }
+
+    return raices;
+}
 
 int main() {
     return 0;
