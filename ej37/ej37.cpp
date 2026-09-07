78df0fa219a08e745a0c545085ea8c6b9c8947f7
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-09-07 15:40:40 -0300

    Ejercicio 17 (untested)

---
+++ ej37/ej37.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+/*
+    37) Escribir un programa que calcule la traza de una matriz cuadrada. Recordar que la traza de una matriz es la suma de los elementos de su diagonal principal.
+*/
+
+float calcularTraza(int** matriz) {
+    int largo = (sizeof(matriz) / sizeof(matriz[0]));
+    float traza = 0;
+
+    for (int i = 0; i < largo; i++){
+        traza += matriz[i][i];
+    }
+
+    return traza;
+}
+
+
+int main() {
+
+    //código
+
+    return 0;
+}
